db7c2c35444a40f65e58dd5b39fcfc150143f792
Ruth3rl <46648485+ruthel@users.noreply.github.com>
2024-11-05 10:17:19 +0100

    update

--- src/pages/api/editor/create_map.js
+++ src/pages/api/editor/create_map.js
@@ -6,14 +6,15 @@ const fs = require('fs');
 export default async function Handler(req, res) {
   try {
     let object = req.body
-    const base64Data = object.data?.replace(/^data:image\/\w+;base64,/, '') || '';
-    const buffer = Buffer.from(base64Data, 'base64');
-    const folderPath = path.join('.', 'uploads');
-    if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath);
-    let image = await Jimp.fromBuffer(buffer)
-    let file_path = path.join(folderPath, `map_${Date.now()}.png`)
-    image.write(file_path).then()
-    object.data = file_path
+    // const base64Data = object.data?.replace(/^data:image\/\w+;base64,/, '') || '';
+    // const buffer = Buffer.from(base64Data, 'base64');
+    // const folderPath = path.join('.', 'uploads');
+    // if (!fs.existsSync(folderPath)) fs.mkdirSync(folderPath);
+    // let image = await Jimp.fromBuffer(buffer)
+    // let file_path = path.join(folderPath, `map_${Date.now()}.png`)
+    // image.write(file_path).then()
+    // object.data = file_path
+    // object.data = buffer
     let result = JSON.parse(JSON.stringify(await Map.create(object)));
     return res.status(201).json(result)
   } catch (e) {
